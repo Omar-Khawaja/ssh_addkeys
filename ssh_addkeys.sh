5c64962ba7deee4579c2867bc96e6d9a90fbe3f3
root <root@example.com>
2016-10-03 14:47:54 -0400

    update

--- ssh_addkeys.sh
+++ ssh_addkeys.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Replace nodes.txt and password.txt with your own files, respectively
-
 file=nodes.txt
 password=password.txt
 
 exec 3<$file
 while read line <&3; do
-	sshpass -f $password ssh -o StrictHostKeyChecking=no $line
+	sshpass -f $password ssh-copy-id -o StrictHostKeyChecking=no $line
 done
